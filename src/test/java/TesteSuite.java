a1d22b278ec6d1ca7595c556d35f83ee238c6526
richardunisul <103585524+richardunisul@users.noreply.github.com>
2022-05-18 12:05:36 -0300

    Update TesteSuite.java
    Arrumando os imports

--- src/test/java/TesteSuite.java
+++ src/test/java/TesteSuite.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit3Suite.java to edit this template
- */
 
-
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 import junit.framework.JUnit4TestAdapter;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+
 import calculadora.TestCalculadora;
 /**
  *
