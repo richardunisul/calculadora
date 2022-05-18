040ad42f674d673ea2d91a30f6ac061f6949afe6
richardunisul <103585524+richardunisul@users.noreply.github.com>
2022-05-18 14:32:14 -0300

    Update TestCalculadora.java
    Atividade Final

--- src/test/java/calculadora/TestCalculadora.java
+++ src/test/java/calculadora/TestCalculadora.java
@@ -28,4 +28,20 @@ public class TestCalculadora {
         double retornoFeito = calculadora.getSubtracao();
         assertEquals(retornoEsperado, retornoFeito, 0);
     }
+
+      @Test
+    public void testGetProduto() {
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 8.0;
+        double retornoFeito = calculadora.getProduto();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+
+      @Test
+    public void testGetDivisao() {
+        Calculadora calculadora = new Calculadora(6.0, 2.0);
+        double retornoEsperado = 3.0;
+        double retornoFeito = calculadora.getDivisao();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }    
 }
