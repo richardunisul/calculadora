bea21e69bebc45e0f4675e161e5e749cdb4f1295
richardunisul <103585524+richardunisul@users.noreply.github.com>
2022-05-18 12:06:26 -0300

    Update TestCalculadora.java
    Arrumando os imports

--- src/test/java/calculadora/TestCalculadora.java
+++ src/test/java/calculadora/TestCalculadora.java
@@ -4,13 +4,8 @@
  */
 package calculadora;
 
-import calculadora.Calculadora;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 /**
  *
