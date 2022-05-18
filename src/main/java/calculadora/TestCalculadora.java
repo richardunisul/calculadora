40f8388b307ddfe7eb535422ab43cc17ef80eaaf
richardunisul <103585524+richardunisul@users.noreply.github.com>
2022-05-18 11:45:27 -0300

    Add files via upload

---
+++ src/main/java/calculadora/TestCalculadora.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package test.java.calculadora;
+
+import calculadora.Calculadora;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author RIHS
+ */
+public class TestCalculadora {
+    
+  @Test
+    public void testGetAdicao() {
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 6.0;
+        double retornoFeito = calculadora.getAdicao();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+}
